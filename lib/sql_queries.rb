589ac6e2472fa1d02650df73721c68a00238a86c
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-06-30 18:35:09 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,22 +7,27 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
-  "SELECT projects.title, SUM(pledges.amount) FROM projects LEFT OUTER JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.id ORDER BY projects.title;"
+  "SELECT projects.title, SUM(pledges.amount) FROM projects 
+   LEFT OUTER JOIN pledges ON projects.id = pledges.project_id 
+   GROUP BY projects.id ORDER BY projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
   "SELECT users.name, users.age, SUM(pledges.amount) FROM users 
-    LEFT OUTER JOIN pledges ON users.id = pledges.user_id GROUP BY users.id ORDER BY users.name;"
+   LEFT OUTER JOIN pledges ON users.id = pledges.user_id 
+   GROUP BY users.id ORDER BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   "SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) AS remaining_amount FROM projects 
-    LEFT OUTER JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title HAVING remaining_amount >= 0;"
+    LEFT OUTER JOIN pledges ON projects.id = pledges.project_id 
+    GROUP BY projects.title HAVING remaining_amount >= 0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
   "SELECT users.name, SUM(pledges.amount) AS summed_amount FROM users 
-   LEFT OUTER JOIN pledges ON users.id = pledges.user_id GROUP BY users.name ORDER BY summed_amount;"
+   LEFT OUTER JOIN pledges ON users.id = pledges.user_id 
+   GROUP BY users.name ORDER BY summed_amount;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
