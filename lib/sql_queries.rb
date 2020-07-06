488feea51e7f8b8ad1183b39a76ad577c7eeb7e3
ryancdavey1 <ryancdavey1@gmail.com>
2020-07-05 21:18:34 -0600

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,19 +23,18 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT projects.title, SUM(pledges.amount) - projects.funding_goal FROM projects
+  "SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) AS amount_over FROM projects
   INNER JOIN pledges
   ON pledges.project_id = projects.id
-  WHERE SUM(pledges.amount) >= projects.funding_goal
-  GROUP BY projects.id
-  ORDER BY projects.title ASC;"
+  GROUP BY projects.title
+  HAVING amount_over >= 0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
   "SELECT users.name, SUM(pledges.amount) FROM users
   INNER JOIN pledges
   ON pledges.user_id = users.id
-  GROUP BY users.id
+  GROUP BY users.name
   ORDER BY SUM(pledges.amount) ASC;"
 end
 
